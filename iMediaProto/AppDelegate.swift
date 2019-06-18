d47b10fe2cb3a84c2d200c6dc468d7425174f315
Habib <habib.rehhman@gmail.com>
2019-06-18 05:14:10 -0700

    initial commit. Pushing to GitHub

--- iMediaProto/AppDelegate.swift
+++ iMediaProto/AppDelegate.swift
@@ -1,21 +1,22 @@
 //
 //  AppDelegate.swift
-//  iMediaProto
+//  Snapchat Camera
 //
-//  Created by a on 6/18/19.
-//  Copyright © 2019 a. All rights reserved.
+//  Created by ashika shanthi on 2/17/18.
+//  Copyright © 2018 ashika shanthi. All rights reserved.
 //
 
 import UIKit
-
+//import Firebase
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+    
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions:
+        [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        FirebaseApp.configure()
         return true
     }
 
