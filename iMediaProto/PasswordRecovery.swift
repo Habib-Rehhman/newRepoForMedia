af098e61f33f201a0ae7624b493a0c6e78b52320
Habib <habib.rehhman@gmail.com>
2019-06-24 15:59:20 +0400

    Added the completed "password recover " screen completed by the UILayoutHelper repository. Added alert dialog triggered by the continue button of this screen that upon pressed 'ok' call the segue to present log in screen again.

---
+++ iMediaProto/PasswordRecovery.swift
@@ -0,0 +1,33 @@
+//
+//  PasswordRecovery.swift
+//  iMediaProto
+//
+//  Created by Habib on 6/24/19.
+//  Copyright © 2019 a. All rights reserved.
+//
+
+
+import UIKit
+
+class PasswordRecoverController: UIViewController {
+
+    
+
+    @IBAction func continuePressed(_ sender: Any) {
+        
+        let alertController = UIAlertController(title: "Recover", message: "A link has been sent to this email. Go to your email and follow the instructions", preferredStyle: .alert)
+        let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: {_ in
+            self.performSegue(withIdentifier: "loginScreen", sender: self)
+        })
+        
+        alertController.addAction(defaultAction)
+        self.present(alertController, animated: true, completion: nil)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+}
